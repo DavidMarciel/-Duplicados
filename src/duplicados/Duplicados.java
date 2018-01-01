52d6320d71316e934953f541c467e03057ccd58d
Marciel <davidmarciel@hotmail.com>
2018-01-01 17:52:20 +0100

    upload

---
+++ src/duplicados/Duplicados.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package duplicados;
+
+/**
+ *
+ * @author david
+ */
+public class Duplicados {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Ventana v = new Ventana();
+        v.setVisible(true);
+        
+        
+    }
+}
